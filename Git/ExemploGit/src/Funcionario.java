3e2c2e3d31f8eea4852d4eeba1c122ce21650396
Auto Logon <guilherme.sanchez1910@gmail.com>
2024-06-03 20:23:35 -0300

    Funcionario atualizado

--- Git/ExemploGit/src/Funcionario.java
+++ Git/ExemploGit/src/Funcionario.java
@@ -9,4 +9,27 @@ public class Funcionario {
         this.nome = nome;
         this.matricula = matricula;
     }
+
+    
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Matricula: " + matricula + "\nNome: " + nome;
+    }
 }

4bccadff3cd59d33148fd056381cfa5071c61f1e
Auto Logon <guilherme.sanchez1910@gmail.com>
2024-06-03 20:16:06 -0300

    sistema criado

---
+++ Git/ExemploGit/src/Sistema.java
@@ -0,0 +1,10 @@
+public class Sistema {
+    
+    private static void exibirMenu() {
+
+        System.out.println("\nCadastro de Funcionários\n");
+        System.out.println("1) Cadastrar Funcionário");
+        System.out.println("2) Exibir dados");
+
+    }
+}

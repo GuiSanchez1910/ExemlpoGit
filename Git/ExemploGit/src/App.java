87a22ad99a6f01cb84bace8e10316374e8ec0e68
Auto Logon <guilherme.sanchez1910@gmail.com>
2024-06-03 20:19:06 -0300

    Funcionario criado

--- Git/ExemploGit/src/App.java
+++ Git/ExemploGit/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Salve o Corinthians");
+        System.out.println("Hello, World!");
     }
 }
---
+++ Git/ExemploGit/src/Funcionario.java
@@ -0,0 +1,12 @@
+public class Funcionario {
+    
+    int matricula;
+    String nome;
+
+    public Funcionario() {}
+
+    public Funcionario(int matricula, String nome) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+}
